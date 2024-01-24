0eb325f8854e9d674603e45fd8e1f0025628542d
ASDomoroschenov <sashachicha228@mail.ru>
2024-01-24 22:06:03 +0300

    refactor Task1

--- SberTask/src/main/java/ru/sber/service/CityService.java
+++ SberTask/src/main/java/ru/sber/service/CityService.java
@@ -6,33 +6,35 @@ import ru.sber.City;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 import java.util.Scanner;
 
 @Slf4j
 public class CityService {
+    private City getCity(String line) {
+        Scanner scanner = new Scanner(line);
+        City city = new City();
+
+        scanner.useDelimiter(";");
+
+        for (int i = 0; i < 6 && scanner.hasNext(); i++) {
+            switch (i) {
+                case 0 -> city.setId(scanner.nextLong());
+                case 1 -> city.setName(scanner.next());
+                case 2 -> city.setRegion(scanner.next());
+                case 3 -> city.setDistrict(scanner.next());
+                case 4 -> city.setPopulation(scanner.nextLong());
+                case 5 -> city.setFoundation(scanner.next());
+            }
+        }
+        return city;
+    }
+
     public void outputListCityFromCSV(String fileNameCSV) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileNameCSV))) {
             String line = null;
 
             while ((line = reader.readLine()) != null) {
-                Scanner scanner = new Scanner(line);
-                City city = new City();
-
-                scanner.useDelimiter(";");
-
-                for (int i = 0; i < 6 && scanner.hasNext(); i++) {
-                    switch (i) {
-                        case 0 -> city.setId(scanner.nextLong());
-                        case 1 -> city.setName(scanner.next());
-                        case 2 -> city.setRegion(scanner.next());
-                        case 3 -> city.setDistrict(scanner.next());
-                        case 4 -> city.setPopulation(scanner.nextLong());
-                        case 5 -> city.setFoundation(scanner.next());
-                    }
-                }
-
-                System.out.println(city);
+                System.out.println(getCity(line));
             }
 
         } catch (IOException | NumberFormatException e) {
