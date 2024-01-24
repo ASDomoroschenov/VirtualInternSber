415fef65c8bf45e729cb29f1fec9de09c7288f8e
ASDomoroschenov <sashachicha228@mail.ru>
2024-01-24 22:04:51 +0300

    Task2 start

--- SberTask/src/main/java/ru/sber/Main.java
+++ SberTask/src/main/java/ru/sber/Main.java
@@ -1,10 +1,7 @@
 package ru.sber;
 
-import ru.sber.service.CityService;
-
 public class Main {
     public static void main(String[] args) {
-        CityService service = new CityService();
-        service.outputListCityFromCSV(Main.class.getClassLoader().getResource("test.csv").getPath());
+
     }
 }
